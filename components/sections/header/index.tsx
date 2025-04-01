1d3bfa8c51a211d520f078b3718c174ab808ff50
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-04-01 10:44:03 +0330

    header items

--- components/sections/header/index.tsx
+++ components/sections/header/index.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button";
 import MenuButton from "./menu-button";
+import { menuItems } from "./contants";
 
 const Header = () => {
   return (
     <div className="py-3">
       <div className="hidden sm:block">
-        
+        {menuItems.map((item, index) => (
+          <Button key={index} variant="ghost">
+            {item.name}
+          </Button>
+        ))}
       </div>
       <MenuButton />
     </div>
