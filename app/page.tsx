e9a9419aed4c4c8e9bf84f689c9f703387836610
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-04-02 11:32:03 +0330

    styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Gallery from "@/components/sections/gallery";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-10 custom-container">
+    <div className="flex flex-col gap-10 sm:gap-15 custom-container">
       <Gallery />
       <Classes />
     </div>
--- components/shared/section-wrapper.tsx
+++ components/shared/section-wrapper.tsx
@@ -7,8 +7,8 @@ interface Params {
 
 const SectionWrapper = ({ title, children }: Params) => {
   return (
-    <div className="flex flex-col gap-4">
-      <h3 className="text-2xl font-bold">{title}</h3>
+    <div className="flex flex-col gap-5">
+      <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold">{title}</h3>
       {children}
     </div>
   );
