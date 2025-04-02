e17071dcd89ab113d069246f2bb3fa7b9d564691
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-04-02 11:29:44 +0330

    styles

--- components/sections/classes/class-card.tsx
+++ components/sections/classes/class-card.tsx
@@ -13,7 +13,7 @@ const ClassCard = ({
   time,
 }: ClassType) => {
   return (
-    <div className="flex flex-col sm:flex-row gap-2 border-4 bg-muted rounded-xl overflow-hidden">
+    <div className="flex flex-col sm:flex-row gap-2 border-4 bg-muted/60 rounded-xl overflow-hidden">
       <div className="flex justify-center items-center">
         <Image src={image} alt={name} width={400} height={400} />
       </div>
@@ -21,18 +21,18 @@ const ClassCard = ({
       <div className="flex flex-col p-4 gap-1 justify-between">
         <div>
           <h4 className="text-xl font-semibold">{name}</h4>
-          <h4 className="lg:w-1/2 font-medium text-foreground/80">
+          <h4 className="lg:w-1/2 font-medium text-foreground/90">
             {description}
           </h4>
         </div>
 
         <div className="my-2">
           {status === "open" ? (
-            <Badge className="!text-sm">
+            <Badge className="!text-sm font-bold">
               تاریخ برگذاری - {date} - ساعت {time}
             </Badge>
           ) : (
-            <Badge className="!text-sm" variant="destructive">
+            <Badge className="!text-sm font-bold" variant="destructive">
               به اتمام رسیده است
             </Badge>
           )}
@@ -40,7 +40,7 @@ const ClassCard = ({
 
         <div className="flex gap-1 mt-2">
           {tags.map((i, index) => (
-            <Badge key={index} variant="secondary">
+            <Badge key={index} variant="secondary" className="text-white font-semibold">
               {i}
             </Badge>
           ))}
