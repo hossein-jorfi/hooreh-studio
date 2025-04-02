924c1c144b6c3a7dc3f74c47d28fbf2cdd647ad7
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-04-02 13:04:07 +0330

    comments

--- components/sections/your-comments/comment-card.tsx
+++ components/sections/your-comments/comment-card.tsx
@@ -1,17 +1,12 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { comments } from "./contants";
+import { CircleUserRound } from "lucide-react";
 
 const CommentCard = ({ comment }: { comment: (typeof comments)[0] }) => (
-  <Card className="h-full border-4 shadow-none rounded-3xl">
-    <CardHeader className="flex flex-row items-center gap-4 ">
-      <Avatar>
-        <AvatarImage src={""} alt={comment.author} />
-        <AvatarFallback>{comment.initials}</AvatarFallback>
-      </Avatar>
-      <div className="flex flex-col">
-        <p className="font-medium">{comment.author}</p>
-      </div>
+  <Card className="h-full border-4 shadow-none rounded-3xl text-right">
+    <CardHeader className="flex flex-row-reverse md:flex-row items-center gap-4 ">
+      <CircleUserRound strokeWidth={1.5} className="size-8" />
+      <p className="font-medium text-lg">{comment.author}</p>
     </CardHeader>
     <CardContent className=" pt-0">
       <p className="text-sm font-medium">{comment.content}</p>
