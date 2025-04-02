4d120cbd1f50b5f2249cf250f42072477af84535
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-04-02 12:10:20 +0330

    faq items

--- components/sections/faq/index.tsx
+++ components/sections/faq/index.tsx
@@ -5,30 +5,18 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { faqItems } from "./contants";
 
 const Faq = () => {
   return (
     <SectionWrapper title="سوالات متداول">
       <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>Is it styled?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It comes with default styles that matches the other
-            components&apos; aesthetic.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>Is it animated?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It's animated by default, but you can disable it if you prefer.
-          </AccordionContent>
-        </AccordionItem>
+        {faqItems.map((item, index) => (
+          <AccordionItem key={index} value={String(index)}>
+            <AccordionTrigger>{item.title}</AccordionTrigger>
+            <AccordionContent>{item.content}</AccordionContent>
+          </AccordionItem>
+        ))}
       </Accordion>
     </SectionWrapper>
   );
