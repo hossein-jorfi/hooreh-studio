77a58c9582756b6382d6505047e6b5d6098e119a
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-04-03 21:55:49 +0330

    edit ClassCard

--- components/sections/classes/class-card.tsx
+++ components/sections/classes/class-card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { ClassType } from "./constants";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { BadgeCheck } from "lucide-react";
 
 const ClassCard = ({
   name,
@@ -10,6 +11,7 @@ const ClassCard = ({
   tags,
   status,
   technics,
+  note,
 }: ClassType) => {
   return (
     <div className="flex flex-col sm:flex-row gap-2 border-4 bg-muted/60 rounded-xl overflow-hidden">
@@ -17,15 +19,24 @@ const ClassCard = ({
         <Image src={image} alt={name} width={400} height={400} />
       </div>
 
-      <div className="flex flex-col p-4 gap-1 justify-between">
-        <div>
+      <div className="flex flex-col p-4 gap-3 justify-between">
+        <div className="flex flex-col gap-1">
           <h4 className="text-xl font-semibold">{name}</h4>
           <h4 className="lg:w-1/2 font-medium text-foreground/90">
             {description}
           </h4>
         </div>
 
-        <div className="my-2 flex flex-col gap-1">
+        {note && (
+          <p className="text-black/70 flex items-start gap-1 font-medium text-sm">
+            <BadgeCheck strokeWidth={2.3} className="size-6 sm:size-4" />
+            <span>
+              {note}
+            </span>
+          </p>
+        )}
+
+        <div className="flex flex-col gap-1">
           {technics && (
             <>
               <p className="text-sm font-bold">تکنیک های آموزشی:</p>
@@ -48,7 +59,7 @@ const ClassCard = ({
           )}
         </div>
 
-        <div className="flex gap-1 mt-2">
+        <div className="flex gap-1">
           {tags.map((i, index) => (
             <Badge
               key={index}
