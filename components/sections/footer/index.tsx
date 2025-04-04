7d67f2119d20e7f78c05cf5d9ce686e588642315
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-04-04 13:57:31 +0330

    footer

--- components/sections/footer/index.tsx
+++ components/sections/footer/index.tsx
@@ -3,19 +3,21 @@ import SocialMediaItem from "./social-media-item";
 
 const Footer = () => {
   return (
-    <div id="contact" className="pattern custom-container p-5">
-      <div className="w-full border-black/30 border-2 p-5 bg-secondary/20 backdrop-blur-xs rounded-2xl">
-        <div className="flex flex-col gap-1">
-          <h4 className="text-xl font-bold">تماس با ما</h4>
-          <p className="font-bold text-foreground/80 text-xs">
-            برای ثبت نام میتوانید یکی از راه های ارتباطی زیر را انتخاب کنید
-          </p>
-        </div>
-        <div className="flex justify-center items-center">
-          <div className="flex flex-col gap-2 mt-4 items-end">
-            {socialItems.map((item, index) => (
-              <SocialMediaItem key={index} {...item} />
-            ))}
+    <div className="pattern">
+      <div id="contact" className="custom-container p-5">
+        <div className="w-full border-black/30 border-2 p-5 bg-secondary/20 backdrop-blur-xs rounded-2xl">
+          <div className="flex flex-col gap-1">
+            <h4 className="text-xl font-bold">تماس با ما</h4>
+            <p className="font-bold text-foreground/80 text-xs">
+              برای ثبت نام میتوانید یکی از راه های ارتباطی زیر را انتخاب کنید
+            </p>
+          </div>
+          <div className="flex justify-center items-center">
+            <div className="flex flex-col gap-2 mt-4 items-end">
+              {socialItems.map((item, index) => (
+                <SocialMediaItem key={index} {...item} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- components/sections/footer/social-media-item.tsx
+++ components/sections/footer/social-media-item.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import { socialItems } from "./contants";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 const SocialMediaItem = ({ icon, text, link }: (typeof socialItems)[0]) => {
   return (
-    <div className="flex gap-3 items-center">
-      <Link target="_blank" href={link}>{text}</Link>
-      <div className="">{icon}</div>
-    </div>
+    <Link target="_blank" href={link} className="w-full flex gap-2">
+      <Button size="icon"><ArrowRight /></Button>
+      <div className="flex gap-2 items-center font-semibold text-foreground bg-foreground/20 px-2 py-1 rounded-lg border-2 border-foreground/80 w-full justify-end">
+        <div>
+          {text}
+        </div>
+        <div>{icon}</div>
+      </div>
+    </Link>
   );
 };
 
