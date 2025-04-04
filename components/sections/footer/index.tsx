7d6fbcfa0ab2dd945471a92f3c685e56ef23f760
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-04-04 12:43:44 +0330

    footer

--- components/sections/footer/index.tsx
+++ components/sections/footer/index.tsx
@@ -1,9 +1,16 @@
 const Footer = () => {
-      return (
-            <div id="contact">
-                  footer
-            </div>
-      );
+  return (
+    <div id="contact" className="pattern custom-container p-5">
+      <div className="w-full border-black/30 border-2 p-5 bg-secondary/20 backdrop-blur-xs rounded-2xl">
+        <div className="flex flex-col gap-1">
+          <h4 className="text-xl font-bold">تماس با ما</h4>
+          <p className="font-bold text-foreground/80 text-xs">
+            برای ثبت نام میتوانید یکی از راه های ارتباطی زیر را انتخاب کنید
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
